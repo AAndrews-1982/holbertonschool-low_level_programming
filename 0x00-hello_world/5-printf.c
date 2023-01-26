2058789f379d9e65b337c4e68cdccd0055721370
AAndrews-1982 <altonandrews@icloud.com>
2023-01-26 12:46:15 -0800

    Hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,12 @@
-/*using printf
 #include <stdio.h>
-
-int main(void)
+/**
+*main - printf to display text in terminal.
+*
+*  Return: 0
+*/  
+intmain(void)
         {
-	printf("With proper grammer, but the outcome is a piece of art,\n);
+	printf("With proper grammer, but the outcome is a piece of art,\n");
 	
 	return (0);
 }
