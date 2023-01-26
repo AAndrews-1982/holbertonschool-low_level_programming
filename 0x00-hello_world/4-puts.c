0221f00a5eab9ef1326a4bc6981d6c16589021ff
AAndrews-1982 <altonandrews@icloud.com>
2023-01-26 13:57:03 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
-
 /**
  * main - prints line of text using puts
  *
- * Return: Always 0 (Success)
+ * Return: 0
  */
-
 int main(void)
 	{
-	puts("\n"Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle");
 	return (0);
 }
