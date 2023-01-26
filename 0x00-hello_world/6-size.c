875789024d77c30ed83b6106741777506d46fc73
AAndrews-1982 <altonandrews@icloud.com>
2023-01-26 14:37:09 -0800

    Size is not grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,20 +1,19 @@
-#include<stdio.h>
+#include <stdio.h>
 /**
  * main - display various types
  *
  * Return: 0
+ */
 int main(void) 
 	{
 
 	int i;
 	double d;
 	char c;
-
-	//evaluates the size of a variable
 	
-	printf("Size of int is: lu.\n", (unsigned long)sizeof(i))"
+	printf("Size of int is: %lu.\n", (unsigned long)sizeof(i))"
 	printf("Size of double is: %lu.\n", (usigned long)sizeof(d));
-	printf("Size of double is: %zu bytes\n", (unsigned longs)sizrof(d));
+	printf("Size of double is: %lu.\n", (unsigned longs)sizeof(d));
 
 	return (0);
 	}	
