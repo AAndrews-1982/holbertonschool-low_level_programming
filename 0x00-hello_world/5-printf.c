7dd6b6806331c158a7f07a92d2edc06b9f1546b3
AAndrews-1982 <altonandrews@icloud.com>
2023-01-27 10:01:21 -0800

    Size is grandeur

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - using printf to display text to terminal.
+ *
+ * Return: 0
+ */
 int main()
-{
+	{
 	printf("With proper grammar, but the outcome is a piece of art,/n")
-	return 0;
-}
+	
+	return (0);
+	}
