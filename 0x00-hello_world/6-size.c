080ca0753013a6edfabe7cf9fab68d4e0a91f334
AAndrews-1982 <altonandrews@icloud.com>
2023-01-28 14:43:00 -0800

    Size is grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,12 +6,16 @@
  */
 	int main(void)
 {
-	int i;
-	double d;
-	char c;
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
 
-	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
-	printf("The size of a double is: %lu.\n", (unsigned long)sizeof(d));
-	printf("The size of a c is: %lu.\n", (unsigned long)sizeof(c));
+	printf("size of char: %lu byte(s)\n", (unsigned long)sizeofa(a));
+	printf("size of int: %lu bytes(s)\n", (unsigned long)sizeof(b));
+	printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+	printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
