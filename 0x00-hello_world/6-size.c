1022ba6a19d0a1a1908554e2866986347f918b3b
AAndrews-1982 <altonandrews@icloud.com>
2023-01-28 14:48:55 -0800

    Size is grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,18 +4,19 @@
  *
  * Return: 0
  */
-	int main(void)
-{
+int main(void)
+	{
+	
 	char a;
 	int b;
 	long int c;
 	long long int d;
 	float e;
 
-	printf("size of char: %lu byte(s)\n", (unsigned long)sizeofa(a));
+	printf("size of char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("size of int: %lu bytes(s)\n", (unsigned long)sizeof(b));
 	printf("size of long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 	printf("size of long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
 	printf("size of float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
-}
+	}
