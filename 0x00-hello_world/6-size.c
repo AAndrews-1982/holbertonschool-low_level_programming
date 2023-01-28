cd5e0d6813f9bf738133061581ebf419cecd8120
AAndrews-1982 <altonandrews@icloud.com>
2023-01-27 17:20:24 -0800

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -4,14 +4,14 @@
  *
  * Return: 0
  */
-	int main (void)
+	int main(void)
 {
 	int i;
 	double d;
 	char c;
 
 	printf("The size of an int is: %lu.\n", (unsigned long)sizeof(i));
-	printf('The size of a double is: %lu.\n", (unsigned long)sizeof(d));
-	printf('The size of a c is: %lu.\n", (unsigned long)sizeof(c));
+	printf("The size of a double is: %lu.\n", (unsigned long)sizeof(d));
+	printf("The size of a c is: %lu.\n", (unsigned long)sizeof(c));
 	return (0);
 }
