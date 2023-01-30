7b988dae68303939d1f0e6dd6ed8d77cf70461f1
AAndrews-1982 <altonandrews@icloud.com>
2023-01-30 11:57:11 -0800

    function that print the alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Function that prints the alphabet in lowercase.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	for(int x = 'a'; x <= 'z'; x++)
+	{
+		putchar(x);
+	}
+	return 0;
+}
