65fb695843f87241ae8e3d78b6125634c24575f3
AAndrews-1982 <altonandrews@icloud.com>
2023-01-29 18:30:36 -0800

    Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -5,14 +5,10 @@
  *
  * Return: 0
  */
-int main(void)
-{
-	int x;
-
-	for (x = 0; x <= 9; x++)
-}
-printf("%d" , x);
-}
-printf("\n");
-return (0);
+int main(void){
+	for (int i = 0; i <= 9; i++) {
+		putchar(i + '0');
+		putchar('\n');
+	}
+	return (0);
 }
