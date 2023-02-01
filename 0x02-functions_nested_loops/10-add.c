f20699a56e4a5be5e41c0d7fc872d5a2722b41cc
AAndrews-1982 <altonandrews@icloud.com>
2023-02-01 13:31:13 -0800

    functions that adds two integers and returns result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,24 @@
+/*
+ * File 10-add.c
+ * Auth: Alton Andrews
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - add two numbers
+ * @a: input char
+ * @b: input char
+ * Description: add two numbers
+ * Return: Always 0
+ */
+
+int add(int a, int b)
+{
+	int result;
+
+	result = a + b;
+
+	return (result);
+}
