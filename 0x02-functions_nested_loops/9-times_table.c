2e6bdfa71e63623d71842924c3c973e12b65c412
AAndrews-1982 <altonandrews@icloud.com>
2023-02-01 14:10:04 -0800

    function that prints the 9 times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,53 @@
+/*
+ * File 9-times_table.c
+ * Auth: Alton Andrews
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - 9x table
+ * Description: print 9 times table
+ * Return: Always 0
+ */
+
+void times_table(void)
+{
+	int num1;
+	int num2;
+	int results;
+
+	for (num1 = 0 ; num1 <= 9 ; num1++)
+	{
+		for (num2 = 0 ; num2 <= 9 ; num2++)
+		{
+			result = num1 * num2;
+
+			if (result == 0)
+			{
+				if (num2 > 0)
+				{
+					_putchar(' ');
+				}
+				_putchar('0');
+			}
+			else if (result > 0 && result < 10)
+			{
+				_putchar(' ');
+				_putchar(result + '0');
+			}
+			else
+			{
+				_putchar((result / 10) + '0');
+				_putchar((result % 10) + '0');
+			}
+			if (num2 < 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+}
