a5da71240a785df7964833c9c8506a9461213b27
AAndrews-1982 <altonandrews@icloud.com>
2023-02-01 13:46:55 -0800

    print natural numbers from n to 98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+/*
+ * File 11-print_to_98.c
+ * Auth: Alton Andrews
+ */
+
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_to_98 - print to 98
+ * @n: input char
+ * Description: from n to 98 up or down
+ * Return: Always 0
+ */
+
+void print_to_98(int n)
+{
+	if (n < 99)
+	{
+		for (; n <= 98 ; n++)
+		{
+			printf("%d", n);
+			if (n < 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	else
+		for (; n >= 98 ; n--)
+			printf('%d, n);
+			if (n > 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
