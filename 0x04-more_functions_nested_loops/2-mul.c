42f6b6def1c1275a3963fb43a050c2360cd476cb
AAndrews-1982 <altonandrews@icloud.com>
2023-02-04 13:40:01 -0800

    multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+/*
+ * File 2-mul.c
+ * Auth: Alton Andrews
+ */
+
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: digit 1
+ * @b: digit 2
+ * Description: a * b
+ * Return: Product of a & b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
