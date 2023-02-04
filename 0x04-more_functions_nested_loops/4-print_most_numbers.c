aaac66207507f4d12e30c9fbcd9c2fccd4a6b02e
AAndrews-1982 <altonandrews@icloud.com>
2023-02-04 13:52:13 -0800

    Print numbers from 0-9 exclude 2&4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,29 @@
+/*
+ * File 4-print_most_numbers.c
+ * Auth: Alton Andrews
+ */
+
+#include "main.h"
+
+/**
+ * print_most_numbers - print 0 - 9 exclude 2 & 4.
+ * Description: Print numbers 0 - 9 not 2 & 4.
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		if (i != 2 && i != 4)
+		{
+			_putchar((i) + '0');
+		}
+		{
+			_putchar((i) + '0');
+		}
+	}
+	_putchar('\n');
+}
