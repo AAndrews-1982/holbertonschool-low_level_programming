ee7c087ef0be64961042ac97204c6e994b27966f
AAndrews-1982 <altonandrews@icloud.com>
2023-02-04 14:45:23 -0800

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,40 @@
+/*
+ * File: 10-print_triangle.c
+ * Auth: Alton Andrews
+ */
+
+#include "main.h"
+
+/**
+ * print_triangle - Print triangle.
+ * @size: input
+ * Description: Triangle hashtags.
+ * Return: nothing
+ */
+
+void print_triangle(int size)
+{
+	int row;
+	int spaces;
+	int htags;
+
+	if (size > 0)
+	{
+		for (row = 1 ; row <= size ; row++)
+		{
+			for (spaces = (size - row) ; spaces > 0 ; spaces--)
+			{
+				_putchar(' ');
+			}
+			for (hTags = 1 ; hTags <= row ; hTags++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
