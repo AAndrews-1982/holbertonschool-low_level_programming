50679e3bb2f549a627bde92eb76a28e26b909761
AAndrews-1982 <altonandrews@icloud.com>
2023-02-04 13:45:36 -0800

    print numbers from 0-9

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+/*
+ * File 3-print_numbers.c
+ * Auth: Alton Andrews
+ */
+
+#include "main.h"
+
+/**
+ * print_numbers - print 0-9
+ * Description: print numbers 0-9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		_putchar((i) + '0');
+	}
+	_putchar('\n');
+}
