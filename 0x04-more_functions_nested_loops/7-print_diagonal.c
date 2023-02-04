01256155b16d8f197867e81a9c93642675c8b664
AAndrews-1982 <altonandrews@icloud.com>
2023-02-04 14:11:06 -0800

    Draw diagonal line

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,36 @@
+/*
+ * File 7-print_diagonal.c
+ * Auth: Alton Andrews
+ */
+
+#include "main.h"
+
+/**
+ * print_diagonal - prints slashes diagonally
+ * @n: Print line of length diagonally
+ * Description: print slashes
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n > 0)
+	{
+		for (i = 0 ; i < n ; i++)
+		{
+			for (j = 0 ; j < i ; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		putchar('\n');
+	}
+}
