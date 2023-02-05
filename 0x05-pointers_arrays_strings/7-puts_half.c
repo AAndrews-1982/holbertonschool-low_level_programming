79efde237e3c9358b41dce0f7a0c112dd15a0f34
AAndrews-1982 <altonandrews@icloud.com>
2023-02-05 08:55:34 -0800

    Print half of a string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+/**
+ * File: 7-puts_half.c
+ * Auth: Alton Andrews
+ */
+
+#include "main.h"
+
+/**
+ * puts_half - Print half of a string followed by new line to stdout.
+ * @str: String value
+ * Return: 0
+ */
+
+void puts_half(char *str)
+{
+	int length = 0;
+	int i;
+
+	while (str[length] != '\0')
+	{
+		length++;
+	}
+	if (length % 2 == 0)
+	{
+		for (i = length / 2 ; i < length ; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	else
+	{
+		for (i = ((length / 2) + 1) ; i < length ; i++)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
+	
