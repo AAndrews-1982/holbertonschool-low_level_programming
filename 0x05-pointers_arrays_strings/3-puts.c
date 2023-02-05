e8702feb80a1a4a506bc082e62329f46fd05e601
AAndrews-1982 <altonandrews@icloud.com>
2023-02-05 08:06:24 -0800

    Print string followed by a new line to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,23 @@
+/*
+ * File: 3-puts.c
+ * Auth: Alton Andrews
+ */
+
+#include "main.h"
+
+/**
+ * _puts - Prints a string, followed by a new line to stdout.
+ * Return: Nothing
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i++;
+	}
+	_putchar('\n');
+}
