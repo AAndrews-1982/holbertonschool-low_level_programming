1cdd024f14a3d91e7bb98b9daa53085ed56a3ea3
AAndrews-1982 <altonandrews@icloud.com>
2023-02-05 08:28:04 -0800

    Reverse a string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,34 @@
+/*
+ * File: 5-rev_string.c
+ * Auth: Alton Andrews
+ */
+
+#include "main.h"
+
+/**
+ * rev_string - reverse a string.
+ * @s: String value
+ * Return: 0
+ */
+
+void rev_string(char *s)
+{
+	int length = 0;
+	int counter;
+	char letter;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	
+	length--;
+
+	for (counter = 0 ; counter < length ; counter++)
+	{
+		letter = s[length];
+		s[length] = s[counter];
+		s[counter] = letter;
+		length--;
+	}
+}
