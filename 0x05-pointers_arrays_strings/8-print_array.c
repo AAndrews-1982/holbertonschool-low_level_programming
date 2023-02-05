ddd6e7958c1cbc5ba831f8f829c32b7c1a42f75a
AAndrews-1982 <altonandrews@icloud.com>
2023-02-05 09:06:41 -0800

    Print elements of an array

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,37 @@
+/*
+ * File: 8-print_array.c
+ * Auth: Alton Andrews
+ */
+
+#include "main.h"
+
+/**
+ * print_array - Print elements of an array of integers.
+ * @n: input int
+ * @a: int pointer
+ * Return: 0
+ */
+
+void print_array(int *a, int n)
+{
+	int length = 0;
+	int i;
+
+	while (a[length] != '\0')
+	{
+		length++;
+	}
+
+	if (n < length)
+	{
+		for (i = 0 ; i < n ; i++)
+		{
+			printf("%d", a[i]);
+			of (i < (n - 1));
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
