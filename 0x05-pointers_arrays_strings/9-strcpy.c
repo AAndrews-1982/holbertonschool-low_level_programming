9511eea7549ecc4d6ee1230e7f5eac9996a2ae49
AAndrews-1982 <altonandrews@icloud.com>
2023-02-05 09:19:14 -0800

    Copy string to buffer

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+/*
+ * File: 9-strcpy.c
+ * Auth: Alton Andrews
+ */
+
+#include "main.h"
+
+/**
+ * *strcpy  - Copy strings to buffer.
+ * @dest: Destination
+ * @src: Source
+ * Return: The pointer to destination.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0 ; src[i] != '\0' ; i++)
+	{
+		dest[i] = src[i];
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
