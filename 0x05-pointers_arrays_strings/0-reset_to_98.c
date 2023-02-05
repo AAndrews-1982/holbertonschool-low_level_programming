fa6b6dd72f17b3f6af590ed1fb3dcb6d246a54c7
AAndrews-1982 <altonandrews@icloud.com>
2023-02-05 07:41:20 -0800

    Update value that points to 98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,17 @@
+/*
+ * File: 0-reset_to_98.c
+ * Auth: Alton Andrews
+ */
+
+#include "main.h"
+
+/**
+ * reset_to_98 - reset parameter and update to 98
+ * @n: inpur pointer
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,3 @@
+0x05. C - Pointers, arrays and strings.
+
+Using pointers, arrays, and strings to code in c.
