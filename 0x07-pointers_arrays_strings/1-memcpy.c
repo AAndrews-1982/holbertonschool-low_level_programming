25bea4a32103d9e45fa72465b1950c7465e061c7
AAndrews-1982 <altonandrews@icloud.com>
2023-02-12 11:27:19 -0800

    functions that copies memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,27 @@
+/*
+ * File: 1-memcpy.c
+ * Auth: Alton Andrews
+ */
+
+#include "main.h"
+
+/**
+ * *_memcpy - copy memory area
+ * @dest: destination
+ * @src: source
+ * @n: length
+ * Return: a pointer to copy location.
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	char *start = dest;
+
+	while (n--)
+	{
+		*dest = src;
+		src++;
+		dest++;
+	}
+	return (start);
+}
