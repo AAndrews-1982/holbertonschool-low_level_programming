cadd093548e4f897460d8cd64919c9324666a3f6
AAndrews-1982 <altonandrews@icloud.com>
2023-02-18 11:29:56 -0800

    main.h

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,26 @@
+/*
+ * File: 0-puts_recursion.c
+ * Auth: Alton Andrews
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * _puts_recursion - print string followed by a new line
+ * @s: string
+ * Return: 0
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
