919b00d418a267c3343dfe459faa2efd3d119679
AAndrews-1982 <altonandrews@icloud.com>
2023-02-18 14:06:17 -0800

    returns the factorial of a given number

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -14,14 +14,13 @@
 
 int factorial(int n)
 {
-	int fact = 1;
-
-	if (n < 0)
-		return (-1);
+	if (n == 0)
 	{
-		for (int i = 1; i <= n; i++)
-			fact *= i;
-
-		return (fact);
+		return (1);
 	}
+	else if (n < 0)
+	{
+		return (-1);
+	}
+	return (n * factorial(n - 1));
 }
