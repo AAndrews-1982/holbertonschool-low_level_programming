782e5f9d4767e267725c09c792d2cb6ed55fd598
AAndrews-1982 <altonandrews@icloud.com>
2023-02-18 13:30:55 -0800

    print a string in reverse

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,22 @@
+/*
+ * File: 1-print_rev_recursion.c
+ * Auth: Alton Andrews
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - prints string in reverse.
+ * @s: string to reverse
+ * Return: void
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
