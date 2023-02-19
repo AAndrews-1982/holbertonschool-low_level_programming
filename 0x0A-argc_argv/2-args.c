cf54ab419af6df070d05cb09baa47c15a1140fad
AAndrews-1982 <altonandrews@icloud.com>
2023-02-19 09:22:38 -0800

    program that prints all arguments it recieves

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,21 +7,18 @@
 #include <stdio.h>
 
 /**
- * main - prints the number of input args.
+ * main - prints name
  * @argc: argument count.
  * @argv: argument vector for values.
- * @argc[0]: is the function name.
  *
- * Return: Always 0 (Success)
+ * Return: 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
-	(void) argc;/*arg c will not be used*/
-	while (*argv)
+	while (argc--)
 	{
-		printf("%s\n", *argv);
-		argv++;
+		printf("%s\n", *argv++);
 	}
 	return (0);
 }
