ede9b27eaf092e134ad298809f7c93acc9a8617d
AAndrews-1982 <altonandrews@icloud.com>
2023-02-18 19:26:19 -0800

    program that prints the name,followed by a new line

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,28 @@
+/*
+ * File: 0-whatsmyname.c
+ * Auth: Alton Andrews
+ */
+
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * argv[0] is function name. later positions have arguments made
+ *
+ *@argv: program name followed by input arguments.
+ *
+ *@argc: number of input arguments
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+/*we did not forget argc. we choose not to use*/
+	/*main must begin with an int argument if any.*/
+	/*will only take 0 or 2 args. this allows taking 2*/
+	printf("%s\n", argv[0]);
+	return (0);
+}
