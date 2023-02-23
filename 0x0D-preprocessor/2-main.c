feb2aa4edff2fa0afac7bff71fe3f3cf9c8947f6
AAndrews-1982 <altonandrews@icloud.com>
2023-02-22 18:37:21 -0800

    program that prints the name of the file

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,19 @@
+/*
+ * File: 2-main.c
+ * Auth: Alton Andrews
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf(__FILE__ "\n");
+
+	return (0);
+}
+
