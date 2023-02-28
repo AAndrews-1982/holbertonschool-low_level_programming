2fdb7b6df91c492c3702260b2938851cb0e40bec
AAndrews-1982 <altonandrews@icloud.com>
2023-02-27 17:25:16 -0800

    dynamic library

--- 0x0A-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x0A-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-gcc -c -fPIC *.c
+
+# Compile all .c files in the current directory into object files
+gcc -c -Wall -Werror -fpic *.c
+
+# Create the dynamic library from the object files
 gcc -shared -o liball.so *.o
+
+# Clean up object files
+rm *.o
+
