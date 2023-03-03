2d11af4937cf1c684ffa3c84b2dd71aedc128461
AAndrews-1982 <altonandrews@icloud.com>
2023-03-03 14:19:53 -0800

    function that prints a name

--- 0x0F-functions_pointers/0-print_name.c
+++ 0x0F-functions_pointers/0-print_name.c
@@ -9,17 +9,16 @@
 
 /**
  * print_name - prints a name
- * @name: pointer to name string
- * @f: function pointer to apply to name
  *
- * Description: This function takes a pointer to a name string and a function
- * pointer that takes a string as an argument and returns nothing. It applies
- * the function to the name and prints the resulting string.
+ * @name: the name to print
+ * @f: a function pointer to apply to the name
+: *
+ * Return: void
  */
 
 void print_name(char *name, void (*f)(char *))
 {
-	if (name == NULL || f == NULL)
-		return;
-	f(name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
+
