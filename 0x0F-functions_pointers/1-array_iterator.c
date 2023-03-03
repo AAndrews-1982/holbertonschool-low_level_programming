eedb83db0ed7e30893336e5cc250c269e2a28a1d
AAndrews-1982 <altonandrews@icloud.com>
2023-03-03 13:35:15 -0800

    main function code

---
+++ 0x0F-functions_pointers/1-array_iterator.c
@@ -0,0 +1,29 @@
+/*
+ * File: 1-array_iterator
+ * Author: Alton Andrews
+ */
+
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_iterator - execute a func as param on els of arr
+ * @array: int ptr
+ * @size: size_t
+ * @action: func ptr
+ * Return: nothing
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (action == NULL || array == NULL)
+		return;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		action(array[i]);
+	}
+}
