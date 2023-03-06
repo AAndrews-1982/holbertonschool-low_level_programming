7bf13ece48f26b11c20d993b1c5950396ca81aa3
AAndrews-1982 <altonandrews@icloud.com>
2023-03-06 11:57:10 -0800

    function that prints a name

--- 0x0F-functions_pointers/0-print_name.c
+++ 0x0F-functions_pointers/0-print_name.c
@@ -9,7 +9,7 @@
 
 /**
  * print_name - prints a name
- *@name: the name to print
+ * @name: the name to print
  * @f: a function pointer to apply to the name
  * Return: nothing
  */
@@ -20,4 +20,3 @@ void print_name(char *name, void (*f)(char *))
 		return;
 	f(name);
 }
-
