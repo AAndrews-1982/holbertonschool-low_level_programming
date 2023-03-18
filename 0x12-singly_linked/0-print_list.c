fa4725630ebefa486d7041b3af0e9ccfd07bb50e
AAndrews-1982 <altonandrews@icloud.com>
2023-03-18 16:33:41 -0700

    fixed code errors

--- 0x12-singly_linked/0-print_list.c
+++ 0x12-singly_linked/0-print_list.c
@@ -1,26 +1,35 @@
+/*
+ * File: 0-print_list.c
+ * Auth: Alton Andrews
+ */
+
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_list - prints all the elements of a list_t list
+ * print_list - print all the elements of a list_t list
  * @h: pointer to head of list_t list
- *
  * Return: the number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+	size_t i = 0;
 
-	while (h != NULL)
+	while (h)
 	{
-		/* check if str is NULL */
 		if (h->str == NULL)
+		{
 			printf("[0] (nil)\n");
+		}
 		else
+		{
 			printf("[%u] %s\n", h->len, h->str);
 
-		count++;
+		}
 		h = h->next;
+		i++;
 	}
-
-	return (count);
+	return (i);
 }
