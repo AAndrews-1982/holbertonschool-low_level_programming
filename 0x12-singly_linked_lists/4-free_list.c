5476b61ca1a7a144749bdab3f272bd09a988cf89
AAndrews-1982 <altonandrews@icloud.com>
2023-03-18 17:21:54 -0700

    Write a function that frees a list_t list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,30 @@
+/*
+ * File: 4-free_lists.c
+ * Auth: Alton Andrews
+ */
+
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_list - free a list
+ * @head: first node
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	temp = head;
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+	free(head);
+}
