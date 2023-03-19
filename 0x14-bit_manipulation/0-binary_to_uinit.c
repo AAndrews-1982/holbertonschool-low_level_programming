2f242206fe216699311d767943f7061338d6b4fe
AAndrews-1982 <altonandrews@icloud.com>
2023-03-19 11:39:33 -0700

    Write a function that converts a binary number to an unsigned int

--- 0x14-bit_manipulation/0-binary_to_uinit.c
+++ 0x14-bit_manipulation/0-binary_to_uinit.c
@@ -1,31 +1,37 @@
-#include "main.h"
-#include <stdio.h>
-
 /**
- * binary_to_uint - converts a binary number to an unsigned int.
- * @b: pointer to a string of 0 and 1 chars.
+ * binary_to_uint - converts a binary value to int val
+ * @b: string of 0/1. if it has anything else, complain
+ * Return: the number represented by b
  *
- * Return: the converted number, or 0 if there is one or more chars in the
- * string b that is not 0 or 1, or b is NULL.
+ * binary's 1's place is index 0.
+ * 10's place is index 1
+ *
+ * decimal number represented is
+ * sum of each (content of index) x 2^index
  */
+
+#include "main.h"
+#include "_strlen.c"
+#include "_pow.c"
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int result = 0;
+	unsigned int index, pos, len, sum = 0;
 
-	if (b == NULL)
+/*	printf("len is %i\n", len);*/
+	if  (b == NULL)
 		return (0);
-
-	while (*b)
-	{
-		if (*b == '0' || *b == '1')
-		{
-			result <<= 1;
-			result += (*b - '0');
-			b++;
-		}
-		else
+	len = (_strlen(b) - 1);
+	for (pos = 0; b[pos]; pos++)
+	{ /*printf("sum spawned in at %i\n", sum);*/
+		if (b[pos] != '0' && b[pos] != '1')
 			return (0);
+		index = (len - pos);
+		if ((b[pos] - 48) != 0)
+			sum += ((b[pos] - 48) * (_simpow(2, index)));
+		/*converting the char into respective int*/
+		/*will need a separate sorting to use hex/anything including letters*/
+/*		printf("index is %i; pos is %i; sum is %i\n", index, pos, sum);*/
+/*		printf("item was %c which turned into int %i\n", b[pos], b[pos] - 48);*/
 	}
-
-	return (result);
+	return (sum);
 }
