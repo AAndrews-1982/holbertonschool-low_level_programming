fd28aded9a2a242e9d20e2588fdaef198edf0737
“AAndrews-1982 <altonandrews@icloud.com>
2023-12-13 19:28:29 -0600

    linear search revised

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -3,63 +3,19 @@
 #include "search_algos.h"
 
 /**
- * linear_search - Searches for a value in an array using linear search
- * @array: Pointer to the first element of the array to search in
- * @size: Number of elements in the array
- * @value: Value to search for
+ * main - Entry point
  *
- * Return: The first index where the value is located, or -1 if not found
- */
-int linear_search(int *array, size_t size, int value)
-{
-	/* Check if the array is NULL */
-	if (array == NULL)
-		return -1; /* Array is NULL, return -1 indicating value not found */
-
-	/* Iterate through the array elements */
-	for (size_t i = 0; i < size; i++)
-	{
-		/* Check if the current element is equal to the search value */
-		if (array[i] == value)
-			return i; /* Value found, return the index */
-
-		/* Print the values being compared (optional and can be removed) */
-		printf("Comparing %d with %d\n", array[i], value);
-	}
-
-	return -1; /* Value not found in the array */
-}
-
-/**
- * main - Entry point of the program
- *
- * Return: Always 0 (Success)
+ * Return: Always EXIT_SUCCESS
  */
 int main(void)
 {
-	/* Define an array of integers */
-	int arr[] = {5, 10, 15, 20, 25, 30};
+    int array[] = {
+        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
+    };
+    size_t size = sizeof(array) / sizeof(array[0]);
 
-	/* Calculate the number of elements in the array */
-	size_t size = sizeof(arr) / sizeof(arr[0]);
-
-	/* Specify the value to search for */
-	int value_to_search = 20;
-
-	/* Call the linear_search function to find the index of the value */
-	int result = linear_search(arr, size, value_to_search);
-
-	/* Check the result and print a corresponding message */
-	if (result != -1)
-	{
-		/* Value found, print the final result including the index */
-		printf("Value %d found at index %d\n", value_to_search, result);
-	}
-	else
-	{
-		/* Value not found, print a message */
-		printf("Value %d not found in the array\n", value_to_search);
-	}
-
-	return 0;
+    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
+    printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
+    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
+    return (EXIT_SUCCESS);
 }
