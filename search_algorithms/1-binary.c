c551e4b4e4b097c36633fe00944601eb8cfe7d93
“AAndrews-1982 <altonandrews@icloud.com>
2023-12-17 16:33:00 -0600

    2-0 - 6-0

--- search_algorithms/1-binary.c
+++ search_algorithms/1-binary.c
@@ -1,36 +1,38 @@
+#include <stdio.h>
 #include "search_algos.h"
 
 /**
- * binary_search - Searches for a value in a sorted array using binary search
- * @array: Pointer to the first element of the array to search
- * @size: Number of elements in the array
- * @value: The value to search for
+ * binary_search - Searches for a value in a sorted array of integers
+ *                using the Binary search algorithm.
+ * @array: Pointer to the first element of the array to search.
+ * @size: Number of elements in the array.
+ * @value: Value to search for.
  *
- * Return: Index where the value is located, or -1 if not found or if array is NULL
+ * Description: This function implements the binary search algorithm
+ *              to find a specific value in a sorted integer array.
+ *              It prints the current subarray being searched every iteration.
+ *
+ * Return: Index where value is located or -1 if not found or array is NULL.
  */
 int binary_search(int *array, size_t size, int value)
 {
+    int left = 0, right = (int)size - 1, mid, i;
+
     if (array == NULL)
         return (-1);
 
-    int left = 0;
-    int right = size - 1;
-    int mid;
-    int i; // Declare 'i' here
-
     while (left <= right)
     {
-        mid = (left + right) / 2;
+        mid = left + (right - left) / 2;
 
         printf("Searching in array: ");
-        for (i = left; i <= right; i++) // Use 'i' here
+        for (i = left; i <= right; i++)
         {
             printf("%d", array[i]);
             if (i < right)
                 printf(", ");
-            else
-                printf("\n");
         }
+        printf("\n");
 
         if (array[mid] == value)
             return (mid);
@@ -40,5 +42,5 @@ int binary_search(int *array, size_t size, int value)
             right = mid - 1;
     }
 
-    return (-1); // Value not found
+    return (-1);
 }
