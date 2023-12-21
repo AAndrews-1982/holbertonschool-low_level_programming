08679e7d391fdfb7edb149a48ed2e54b6f4e518c
“AAndrews-1982 <altonandrews@icloud.com>
2023-12-20 23:26:46 -0600

    Fixed Betty error

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,12 +1,8 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
-#include <stdio.h>
-#include <stdlib.h>
-#include "search_algos.h"
 
 /* Function prototype for the linear search algorithm */
 int linear_search(int array[], size_t size, int value);
-
-/* You can add more function prototypes for other search algorithms here, if needed */
+int binary_search(int *array, size_t size, int value);
 
 #endif /* SEARCH_ALGOS_H */
